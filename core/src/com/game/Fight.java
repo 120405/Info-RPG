6304d1d90907eb19c05d44c37c6ce0d3d718e329
120405 <rietzsch.christian@gmail.com>
2023-01-11 19:48:34 +0100

    E

---
+++ core/src/com/game/Fight.java
@@ -0,0 +1,23 @@
+package com.game;
+
+public class Fight {
+	private String winner = "";
+	public Fight() {
+		
+	}
+	public String fight(Monster monster, Hero hero) {
+		if(monster.getStatus() && hero.getStatus()) {
+			monster.getAttacked(hero.attack());
+			hero.getAttacked(hero.attack());
+		} else {
+		 if(!monster.getStatus()) {
+			winner = "Hero";
+				
+		}else if(!hero.getStatus()){
+			winner = "Monster";
+			
+		}
+		}
+		return winner;
+	}
+}
---
+++ core/src/com/game/Hero.java
@@ -0,0 +1,75 @@
+package com.game;
+
+public class Hero {
+	 int LP = 0;
+	 int random = 0;
+	 int fullLP = 0;
+	 int ATK = 0;
+	 int x = 0;
+	 int y = 0;
+	 String name = "";
+	 boolean alive = true;
+	public Hero() {
+		
+	}
+	
+	public int attack() {
+		return ATK;
+	}
+	public void getAttacked(int strength) {
+		LP = LP - strength;
+		if(LP < strength) {
+			LP = 0;
+			die();
+		}
+	}
+	public void die() {
+		alive = false;
+	}
+	public void setLP(int LP) {
+		this.LP = LP;
+	}
+	public int getLP() {
+		return LP;
+	}
+	public void setFullLP(int fullLP) {
+		this.fullLP = fullLP;
+	}
+	public int getFullLP() {
+		return fullLP;
+	}
+	public void setATK(int ATK) {
+		this.ATK = ATK;
+	}
+	public int getATK() {
+		return ATK;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setStatus(boolean alive) {
+		this.alive = alive;
+	}
+	public boolean getStatus() {
+		return alive;
+	}
+	public void setRandom(int random) {
+		this.random = random;
+	}
+	public int getRandom() {
+		return random;
+	}
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+}
