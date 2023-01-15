36cd0bf2ee889a40a460b4440a826c35c33786ec
FantasticFisch <flozian2005@gmail.com>
2023-01-15 17:02:03 +0100

    Moved Map to own class

---
+++ core/src/com/game/MapRender.java
@@ -0,0 +1,123 @@
+package com.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class MapRender {
+    static TiledMapTileLayer layer;
+    static float tileSize;
+    static ChainShape cs;
+    static TiledMap tiledmap;
+    public static World world;
+    private OrthogonalTiledMapRenderer tmr;
+    private OrthographicCamera cam;
+    private final SpriteBatch batch;
+
+
+    // Methode, um automatisch die art der Texture zu erkennen und dementsprechende
+    // Collisions zu erstellen
+    public MapRender(SpriteBatch batch) {
+        this.batch = batch;
+        tiledmap = new TmxMapLoader().load("1.tmx");
+        float unitScale = 1 / 16f;
+        tmr = new OrthogonalTiledMapRenderer(tiledmap, unitScale);
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        cam = new OrthographicCamera(20, 20 * (h / w));
+        cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+        cam.update();
+    }
+
+    public static void b2dPlats() {
+
+        layer = (TiledMapTileLayer) tiledmap.getLayers().get(0);
+        tileSize = layer.getTileWidth() / 16f;
+
+
+        // layers durchgehen
+        for (int row = 0; row < layer.getHeight(); row++) {
+            for (int col = 0; col < layer.getWidth(); col++) {
+                TiledMapTileLayer.Cell cell = layer.getCell(col, row);
+
+                if (cell == null) {
+                    continue;
+                }
+                if (cell.getTile() == null) {
+                    continue;
+                }
+
+                // body erstellen
+                BodyDef bdef = new BodyDef();
+                FixtureDef fdef = new FixtureDef();
+                bdef.type = BodyDef.BodyType.StaticBody;
+                bdef.position.set((col + 0.5f) * tileSize, (row + 0.5f) * tileSize);
+
+                // Koordinaten verbinden
+                Vector2[] v = new Vector2[5];
+                v[0] = new Vector2(-tileSize / 2f, -tileSize / 2f);
+                v[1] = new Vector2(-tileSize / 2f, tileSize / 2f);
+                v[2] = new Vector2(tileSize / 2f, tileSize / 2f);
+                v[3] = new Vector2(tileSize / 2f, -tileSize / 2f);
+                v[4] = new Vector2(-tileSize / 2f, -tileSize / 2f);
+                cs = new ChainShape();
+                cs.createChain(v);
+
+                // body einstellungen
+                fdef.friction = 0f;
+                fdef.shape = cs;
+
+                world.createBody(bdef).createFixture(fdef).setUserData("collision");
+
+            }
+
+        }
+    }
+
+    private void handleInput() {
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            cam.translate(-3, 0, 0);
+            //If the LEFT Key is pressed, translate the camera -3 units in the X-Axis
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            cam.translate(3, 0, 0);
+            //If the RIGHT Key is pressed, translate the camera 3 units in the X-Axis
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            cam.translate(0, -3, 0);
+            //If the DOWN Key is pressed, translate the camera -3 units in the Y-Axis
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            cam.translate(0, 3, 0);
+            //If the UP Key is pressed, translate the camera 3 units in the Y-Axis
+        }
+
+
+        float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+        float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+
+        cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
+        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+    }
+
+    public void render() {
+        handleInput();
+        batch.setProjectionMatrix(cam.combined);
+        cam.update();
+        tmr.setView(cam);
+        tmr.render();
+    }
+
+}
--- core/src/com/game/MyScreen.java
+++ core/src/com/game/MyScreen.java
@@ -17,47 +17,36 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyScreen extends ScreenAdapter  {
+public class MyScreen extends ScreenAdapter {
     private final SpriteBatch batch;
     Animator animator;
     Texture img;
-    private OrthographicCamera cam;
-    static TiledMap tiledmap;
-    public static World world;
-    private OrthogonalTiledMapRenderer tmr;
+    MapRender map;
+
 
     public MyScreen(SpriteBatch batch) {
         this.batch = batch;
         create();
     }
+
     public void create() {
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-        cam = new OrthographicCamera(20, 20 * (h / w));
-        cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
-        cam.update();
-
-        tiledmap = new TmxMapLoader().load("1.tmx");
-
-        float unitScale = 1 / 16f;
-        tmr = new OrthogonalTiledMapRenderer(tiledmap, unitScale);
         animator = new Animator();
         img = new Texture("Background.png");
+        map = new MapRender(batch);
 
     }
-    public void render (float delta) {
+
+    public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
         //Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
-        handleInput();
-        cam.update();
 
-        batch.setProjectionMatrix(cam.combined);
+
         batch.begin();
-       // batch.draw(img, 0, 0);
+        // batch.draw(img, 0, 0);
         batch.end();
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-             Spiel.INSTANCE.titleScreen();
+            Spiel.INSTANCE.titleScreen();
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
             Spiel.INSTANCE.shopScreen();
@@ -66,44 +55,16 @@ public class MyScreen extends ScreenAdapter  {
             Spiel.INSTANCE.fightScreen();
         }
         animator.render();
-        tmr.setView(cam);
-        tmr.render();
+        map.render();
+
 
     }
 
-    private void handleInput() {
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            cam.translate(-3, 0, 0);
-            //If the LEFT Key is pressed, translate the camera -3 units in the X-Axis
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            cam.translate(3, 0, 0);
-            //If the RIGHT Key is pressed, translate the camera 3 units in the X-Axis
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            cam.translate(0, -3, 0);
-            //If the DOWN Key is pressed, translate the camera -3 units in the Y-Axis
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            cam.translate(0, 3, 0);
-            //If the UP Key is pressed, translate the camera 3 units in the Y-Axis
-        }
-
-
-
-
-        float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
-        float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
-
-        cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
-        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
-    }
-
-    public void dispose () {
+    public void dispose() {
         batch.dispose();
 
-        if(img != null) {
+        if (img != null) {
             img.dispose();
 
         }
