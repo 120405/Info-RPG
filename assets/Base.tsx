7bb42fe98ea0e26fcc2d302573cef002072867b4
FantasticFisch <flozian2005@gmail.com>
2023-01-17 22:22:14 +0100

    New Map

---
+++ assets/Base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="Base" tilewidth="16" tileheight="16" tilecount="1992" columns="8">
+ <image source="[Base]BaseChip_pipo.png" width="128" height="3984"/>
+</tileset>
