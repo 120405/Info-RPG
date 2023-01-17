72ae3f44f90ab7e745c1cec807261d320935ea2d
120405 <rietzsch.christian@gmail.com>
2023-01-17 19:59:37 +0100

    fix

--- core/src/com/game/FightScreen.java
+++ core/src/com/game/FightScreen.java
@@ -36,6 +36,7 @@ public class FightScreen extends ScreenAdapter {
         public void create() {
             stage = new Stage();
             inventory =  new GUI();
+            inventory.addComponent(new Buttons("Close", stage, "hideInv", 2.21, 2.5, Color.OLIVE));
             img = new Texture("Background.png");
             font = new BitmapFont();
             font.setColor(Color.WHITE);
