28ae1dc9bd286cc3eb5967c51e0bb2856d25ef83
FantasticFisch <flozian2005@gmail.com>
2023-01-18 20:21:33 +0100

    Character with Animation

--- core/src/com/game/Animator.java
+++ core/src/com/game/Animator.java
@@ -2,16 +2,25 @@ package com.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Animator implements ApplicationListener {
     SpriteBatch batch;
-    Texture SRun;
+    Texture knightWalk;
     float stateTime;
-    Animation<TextureRegion> SRunAnim;
+    Animation<TextureRegion> RunUpAnim;
+    Animation<TextureRegion> RunDownAnim;
+    Animation<TextureRegion> RunLeftAnim;
+    Animation<TextureRegion> RunRightAnim;
+    TextureRegion currentFrame;
+
+
+    Sprite knight;
 
 
     public Animator() {
@@ -23,8 +32,11 @@ public class Animator implements ApplicationListener {
         batch = new SpriteBatch();
         stateTime = 0f;
 
-        SRun = new Texture(Gdx.files.internal("SRun.png"));
-        SRunAnim = CreateAnim(SRun, 3, 2,0.5f);
+        knightWalk = new Texture(Gdx.files.internal("Soldier Walk-Sheet.png"));
+        RunDownAnim = CreateAnimRow(knightWalk, 5, 6,0.2f,0);
+        RunUpAnim = CreateAnimRow(knightWalk, 5, 6,0.2f,1);
+        RunRightAnim = CreateAnimRow(knightWalk, 5, 6,0.2f,2);
+        RunLeftAnim = CreateAnimRow(knightWalk, 5, 6,0.2f,3);
     }
 
 
@@ -43,13 +55,54 @@ public class Animator implements ApplicationListener {
         return Anim;
     }
 
+    public Animation<TextureRegion> CreateAnimRow(Texture t, int row, int col,float frameTime,int col1) {
+        Animation<TextureRegion> Anim = null;
+        TextureRegion[][] tmp = TextureRegion.split(t, t.getWidth() / col, t.getHeight() / row);
+        TextureRegion[] TR = new TextureRegion[col * row];
+        int index = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                TR[index++] = tmp[col1][j];
+            }
+            Anim = new Animation<>(frameTime, TR);
+        }
+        return Anim;
+    }
+    public void moveUpdate(){
+         currentFrame = RunUpAnim.getKeyFrame(stateTime, true);
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+           currentFrame = RunLeftAnim.getKeyFrame(stateTime, true);
+        }
+       else if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            currentFrame = RunRightAnim.getKeyFrame(stateTime, true);
+        }
+       else if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            currentFrame = RunUpAnim.getKeyFrame(stateTime, true);
+        }
+       else if(Gdx.input.isKeyPressed(Input.Keys.S)){
+            currentFrame = RunDownAnim.getKeyFrame(stateTime, true);
+        }
+        else {
+            TextureRegion[][] tmp = TextureRegion.split(knightWalk, knightWalk.getWidth() / 6, knightWalk.getHeight() / 5);
+            currentFrame = tmp[4][0];
+        }
+    }
+
 
     @Override
     public void render() {
         stateTime += Gdx.graphics.getDeltaTime();
-        TextureRegion currentFrame = SRunAnim.getKeyFrame(stateTime, true);
+        moveUpdate();
+        knight = new Sprite(currentFrame);
+        knight.setCenter(currentFrame.getRegionWidth()/2f,currentFrame.getRegionHeight()/2f);
+        knight.setOrigin(currentFrame.getRegionWidth()/2f,currentFrame.getRegionHeight()/2f);
+        knight.setPosition(Gdx.graphics.getWidth()/2f-15f,Gdx.graphics.getHeight()/2f+30f);
+        knight.scale(3.3f);
+
+
         batch.begin();
 
+        knight.draw(batch);
 
         batch.end();
     }
@@ -72,7 +125,7 @@ public class Animator implements ApplicationListener {
     @Override
     public void dispose() {
         batch.dispose();
-        SRun.dispose();
+        knightWalk.dispose();
     }
 }
 
