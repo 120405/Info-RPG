ffde24d98aa54cfbb4f32f577eb7ab467d7f480b
Viperv4 <s.kerkenpass@t-online.de>
2023-01-30 19:35:10 +0100

    Effects V1

--- core/src/com/game/Monster.java
+++ core/src/com/game/Monster.java
@@ -8,25 +8,53 @@ public class Monster {
    private int ATK = 0;
    private String name = "";
    private boolean alive = true;
+   private int attackModifier;
+   private int damageModifier;
+   private int effectDuration;
+   private String currentEffect;
 
     public Monster(int LP, int fullLP, int ATK, String name) {
     this.LP = LP;
     this.fullLP = fullLP;
     this.ATK = ATK;
     this.name = name;
+    attackModifier = 0;
+    damageModifier = 0;
+    effectDuration = 0;
+    currentEffect = "";
     }
 
     public int attack() {
-        return ATK + random;
+        return ATK + random + attackModifier;
     }
 
-    public void getAttacked(int strength) {
-        LP = LP - strength;
+    public void getAttacked(int strength, String effect) {
+        LP = LP - strength - damageModifier;
         if (LP < strength) {
             LP = 0;
             die();
         }
+        if (effectDuration > 0) {
+            effectDuration --;
+        }
+        if(effectDuration == 0){
+            attackModifier = 0;
+            damageModifier = 0;
+            currentEffect = "";
+        }
 
+        if(!effect.equals("")) {
+            currentEffect = effect;
+        }
+        if(currentEffect.equals("fire")){
+            effectDuration = 5;
+            damageModifier = 20;
+        }
+        if(currentEffect.equals("poison")){
+            effectDuration = 5;
+            damageModifier = 5;
+            attackModifier = -5;
+        }
     }
 
     public void die() {
@@ -79,5 +107,13 @@ public class Monster {
 
     public int getRandom() {
         return random;
+    }
+
+    public int getDamageModifier() {
+        return damageModifier;
+    }
+
+    public void setDamageModifier(int damageModifier) {
+        this.damageModifier = damageModifier;
     }
 }
