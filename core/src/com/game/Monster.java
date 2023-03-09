1a87a8237599e5a8e5a071720ae1c25d2a4b152b
Viperv4 <s.kerkenpass@t-online.de>
2023-03-09 20:18:12 +0100

    Unlimited Effects V1

--- core/src/com/game/Monster.java
+++ core/src/com/game/Monster.java
@@ -1,7 +1,6 @@
 package com.game;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Monster {
    private int LP = 0;
@@ -33,7 +32,124 @@ public class Monster {
     effectDuration = 0;
     blank = new Effect("",0,0,0);
     currentEffect = blank;
+    effects = new List() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public Object[] toArray(Object[] a) {
+            return new Object[0];
+        }
+
+        @Override
+        public boolean add(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int index, Collection c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Object get(int index) {
+            return null;
+        }
+
+        @Override
+        public Object set(int index, Object element) {
+            return null;
+        }
+
+        @Override
+        public void add(int index, Object element) {
+
+        }
+
+        @Override
+        public Object remove(int index) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator listIterator(int index) {
+            return null;
+        }
+
+        @Override
+        public List subList(int fromIndex, int toIndex) {
+            return null;
+        }
+    };
     }
+
 
     public int attack() {
     	int a =(int) (Math.random()* 100);
@@ -86,6 +202,10 @@ public class Monster {
             LP = 0;
             die();
         }
+
+        if(!effect.getName().equals("")){
+            effects.add(effect);
+        }
         
         if(!effects.isEmpty()) {
          for(int i = 0; i < effects.size(); i++) {
@@ -110,7 +230,7 @@ public class Monster {
         }
 
         if (weaponSkill.equals("doubleAttack")){
-            getAttacked(strength, effect, "");
+            getAttacked(strength, blank, "");
         }
     }
 
