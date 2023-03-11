81bc504361e6d373befe539733675a7ed996fca7
FantasticFisch <flozian2005@gmail.com>
2023-03-11 20:47:26 +0100

    Map Update

---
+++ assets/Forrest Wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="Forrest Wall" tilewidth="16" tileheight="16" tilecount="48" columns="8">
+ <image source="[A]_type3/[A]Wall-Up_Forest1_pipo.png" width="128" height="96"/>
+</tileset>
